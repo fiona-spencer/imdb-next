30ce725442c04fa09d19c03188f8e7a96344a5f4
fiona-spencer <fionagspencer@gmail.com>
2024-03-22 02:15:32 -0400

    dark mode

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,20 +1,26 @@
-import { Inter } from "next/font/google";
-import "./globals.css";
-import Header from "../components/Header";
+import { Inter } from 'next/font/google';
+import './globals.css';
+import Header from '../components/Header';
+import Providers from './Providers';
+//import Navbar from './components/Navbar';
+//import SearchBox from './components/SearchBox';
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata = {
-  title: "IMBD Clone",
-  description: "This is a movie database clone",
+  title: 'IMDb clone',
+  description: 'This is a movie database clone',
 };
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang='en'>
       <body className={inter.className}>
-        <Header/>
-        {children}</body>
+        <Providers>
+          <Header />
+          {children}
+        </Providers>
+      </body>
     </html>
   );
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import React from "react";
 import MenuItem from './MenuItem';
 import { AiFillHome } from 'react-icons/ai';
 import { BsFillInfoCircleFill } from 'react-icons/bs';
-//import DarkModeSwitch from './DarkModeSwitch';
+import DarkModeSwitch from './DarkModeSwitch';
 
 export default function Header() {
   return (
@@ -12,9 +12,12 @@ export default function Header() {
         <MenuItem title="home" address="/home" Icon={AiFillHome}></MenuItem>
         <MenuItem title="about" address="/about" Icon={BsFillInfoCircleFill}></MenuItem>
       </div>
+      <div className='flex items-center gap-4'>
+        <DarkModeSwitch/>
+      </div>
       <Link href={'/'} className='flex gap-1 items-center'>
-        <span className='text-xl font-bold bg-amber-500 py-2 rounded-lg'>IMDb</span>
-        <span className='text-xl hidden sm:inline'>Clone</span>
+        <span className='text-xl font-bold bg-blue-500 p-2 rounded-lg'>IMDb</span>
+        <span className='text-xl font-bold hidden sm:inline'>Clone</span>
       </Link>
     </div>
   );
