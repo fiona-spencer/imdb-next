04c7230c2852aefcdb738a6e67b5c1eb552433a4
fiona-spencer <fionagspencer@gmail.com>
2024-03-22 11:16:43 -0400

    add movie page

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -4,7 +4,7 @@ import { FiThumbsUp } from 'react-icons/fi';
 
 export default function Card({ result }) {
   return (
-    <div className='group cursor-pointer sm:hover:shadow-slate-400 sm:shadow-sm 
+    <div className='group cursor-pointer bg-slate-500 sm:hover:shadow-slate-400 sm:shadow-sm 
     rounded-lg sm:border sm:border-slate-400 sm:m-2
     transition-shadow duration-200'
     >
